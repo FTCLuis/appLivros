4f2efdf91978fc8e723349e785559b03f057f0ae
LuisaoxD <105399037+LuisaoxD@users.noreply.github.com>
2024-04-04 19:53:07 -0300

    alteracoes viewlivro

--- screens/ViewLivro.tsx
+++ screens/ViewLivro.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, StyleSheet } from 'react-native';
 import axios from 'axios';
+
 
 export default function ViewLivro({ route }) {
   const { id } = route.params;
@@ -23,13 +24,87 @@ export default function ViewLivro({ route }) {
     <View>
       {livro && (
         <>
-          <Text>{livro.titulo}</Text>
-          <Text>{livro.ano}</Text>          
-          <Text>{livro.editora}</Text>
-          <Text>{livro.autorPrincipal }</Text>
-          <Image source={{ uri: livro.imagem}} style={{ width: 100, height: 100 }} />
+          <div style={style.container}>
+            <div style={style.card}>
+              <div style={style.cabecalhoLivro}>
+                <Text>
+                  <Text style={style.descricaoTitulo}><Text style={{fontWeight: "bold"}}>Título: </Text>{livro.titulo} </Text>
+                </Text>
+                <Text>
+                  <Text style={style.descricaoAno}><Text style={{fontWeight: "bold", marginLeft: 12}}>Ano: </Text>{livro.ano} </Text>
+                </Text>
+              </div>
+              <div>
+
+              </div>
+              <Text>
+                <Text style={{fontWeight: "bold"}}>Lançamento:</Text> {livro.ano}
+              </Text>      
+              <Text>
+                <Text style={{fontWeight: "bold"}}>Assuntos:</Text> {livro.assuntos}
+              </Text>    
+              <Text>
+                <Text style={{fontWeight: "bold"}}>Editora:</Text> {livro.editora}
+              </Text>
+              <Text>
+                <Text style={{fontWeight: "bold"}}>Autor Principal:</Text> {livro.autorPrincipal}
+              </Text>
+              <Text>
+                <Text style={{fontWeight: "bold"}}>Autores:</Text> {livro.autores}
+              </Text>
+              <Text>
+                <Text style={{fontWeight: "bold"}}>Edição:</Text> {livro.edicao}
+              </Text>
+              <Text>
+                <Text style={{fontWeight: "bold"}}>Id:</Text> {livro.id}
+              </Text>
+              <Text>
+                <Text style={{fontWeight: "bold"}}>Idioma:</Text> {livro.idioma}
+              </Text>
+              <Text>
+                <Text style={{fontWeight: "bold"}}>ISBN/ISSN:</Text> {livro.isbnIssn}
+              </Text>
+              <Text>
+                <Text style={{fontWeight: "bold"}}>Material:</Text> {livro.material}
+              </Text>
+              <Text>
+                <Text style={{fontWeight: "bold"}}>Obra:</Text> {livro.obra}
+              </Text>
+              <Image resizeMode='center' source={{ uri: `https://bibliotecaetecmaua.azurewebsites.net/Content/Images/${livro.imagem}` }} style={{ width: 200, height: 200 }} />
+            </div>
+          </div>
         </>
       )}
     </View>
   );
 }
+
+const style = StyleSheet.create(
+  {
+    container: {
+      width: "50%",
+      margin: "auto",
+      backgroundColor: "lightgray",
+      borderRadius: 20,
+    },
+
+    card: {
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+    },
+
+    cabecalhoLivro: {
+      display: 'flex',
+    },
+
+    descricaoTitulo: {
+      fontSize: 22
+    },
+
+    descricaoAno: {
+      fontSize: 22
+    }
+  }
+)
+
